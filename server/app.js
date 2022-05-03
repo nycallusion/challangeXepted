db2951128a81bfb1b618ddd8e834f365bcdfc415
Dave.L <dlau3335@gmail.com>
2022-05-03 09:56:54 -0400

    added server template

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/', indexRouter);
+router.use('/challengexcepted', indexRouter);
+
+module.exports = app;
